a68c67a61997eaa7a54ff3dc8b912e8547389041
Cborrero1 <Cborrero1@mail.csuchico.edu>
2017-03-31 12:56:58 -0700

    Add files via upload

--- jsproject.js
+++ jsproject.js
@@ -9,7 +9,8 @@ function setCookie(cname, cvalue, exdays) {
 
 function getCookie(cname) {
     var name = cname + "=";
-    var ca = document.cookie.split(';');
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
